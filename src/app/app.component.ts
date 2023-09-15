694058cd9f2688ef17b6b1eb035a9209d2816119
Wangdu03 <jeyeyo8941@recutv.com>
2023-09-15 15:40:59 +0530

    todo-completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,11 @@ export class AppComponent {
   todos = [
     { id: 1, task: 'swimming',venue1:"marena",venue2:"sundar",completed:false },
     { id: 2, task: 'breakfast',venue1:"bread & eggs",venue2:"hostel",completed:false },
-    { id: 3, task: 'office',venue1:"SMIT",venue2:"Out of Campus",completed:false }
+    { id: 3, task: 'college',venue1:"SMIT",venue2:"Online",completed:false },
+    { id: 4, task: 'shopping',venue1:"Singtam",venue2:"Rangpo",completed:false },
+    { id: 5, task: 'reading',venue1:"SMIT Library",venue2:"Hostel",completed:false },
+    { id: 6, task: 'dinner',venue1:"Mess",venue2:"Canteen",completed:false },
+    { id: 3, task: 'screaming-chiyaaaaaahh',venue1:"SMIT",venue2:"Teesta",completed:false }
   ];
   toggleTodoCompleted(todo: any) {
     todo.completed = !todo.completed;
